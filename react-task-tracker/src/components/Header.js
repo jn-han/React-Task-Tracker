23ede7ba6e5504ddda0edd837a9090519511e044
jn-han <joshuahnguyen@gmail.com>
2021-09-19 23:34:30 -0700

    Created header and button components

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = ({ title }) => {
+    const onClick = () =>{
+        console.log("click");
+    }
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Jaiden Yung'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header
