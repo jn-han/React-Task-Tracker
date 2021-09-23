cf5cff83cecaed88ceb5c4e1d42f1a14d9caa3da
jn-han <joshuahnguyen@gmail.com>
2021-09-23 00:24:41 -0700

    Added functions such as add and delete task btn

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,15 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
        <header className='header'>
            <h1>{title}</h1>
-           <Button text='Add' color='green' onClick = {onClick}/>
+           <Button text={showAdd ? 'Close' : 'Add'} 
+           color={showAdd ? 'red' : 'green'} onClick = {onAdd}/>
        </header>
     )
 }
